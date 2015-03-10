43f9e7b2d580df71f56e3e4eff704f5ce302c3ee
Luke Walsh <luke.walsh23@gmail.com>
2015-03-10 15:16:33 -0400

    Up

--- f/purdue.js
+++ f/purdue.js
@@ -1,1 +1,11 @@
-alert('hi');
+alert('ready?');
+
+// Clear the DOM
+document.removeChild(document.documentElement);
+
+// Render in page
+document.body.appendChild(
+  document
+    .createElement('div')
+    .appendChild(document.createTextNode("Boom"))
+  );
